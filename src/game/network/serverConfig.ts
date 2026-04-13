6f4b26036232efdc5fb48f0196b06328835d19ff
DerpyDoggo11 <vrs18451@gmail.com>
2026-04-13 09:52:29 -0700

    update for prod server again

--- src/game/network/serverConfig.ts
+++ src/game/network/serverConfig.ts
@@ -3,14 +3,14 @@ const USE_LOCAL = true;
 const LOCAL_WS  = "ws://localhost:2567";
 const LOCAL_HTTP = "http://localhost:2567";
 
-const PROD_WS  = "wss://your-app-name.onrender.com";
-const PROD_HTTP = "https://your-app-name.onrender.com";
+const PROD_WS  = "wss://open-confict-rts-server.onrender.com";
+const PROD_HTTP = "https://open-confict-rts-server.onrender.com";
 
 const LOCAL_LOBBY = 'http://localhost:5173/';
-const PROD_LOBBY = 'https://your-app-name.pages.dev';
+const PROD_LOBBY = 'https://open-conflict-lobby.pages.dev/';
 
 const LOCAL_GAME = 'http://localhost:5174/';
-const PROD_GAME = 'https://your-app-name.pages.dev';
+const PROD_GAME = 'https://open-conflict-rts.pages.dev/';
 
 export const SERVER_WS_URL  = USE_LOCAL ? LOCAL_WS  : PROD_WS;
 export const SERVER_HTTP_URL = USE_LOCAL ? LOCAL_HTTP : PROD_HTTP;
